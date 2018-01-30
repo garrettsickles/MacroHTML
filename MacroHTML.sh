d9210e184ff54bb2e6f6d651e3cabe074b09a197
Garrett Sickles <garrett.sickles@mapquest.com>
2018-01-29 22:11:30 -0700

    Better CLI

--- MacroHTML.sh
+++ MacroHTML.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
+echo "================ MacroHTML ==============="
+echo "  Generate HTML using the C Preprocessor  "
+echo "=========================================="
+
 # Check for all the sources
 for source in "$@"; do
 	# Check if the file exists
 	if [[ ! -f "$source" ]]; then
 		echo "Error: Could not find $source"
 		exit
+	else
+		echo "Found supplied source: $source"
 	fi
 done
 		#gcc -E $var | sed '/^#/ d' | sed '/^\s*$/d' | tr -d '\n' > test.html
 
 # If there are no arguements, use templates (*.html.h) in current directory
 if [ "$#" -eq 1 ]; then
+	echo "No source supplied"
 	echo "Looking for sources..."
 
 	# for each file in the directory
 	for source in "."/*; do
 		# Check for correct extension
 		if [[ $source =~ \.html.h ]]; then
-			echo "Found source $source"
+			echo "Found source: $source"
 			base="${source/.html.h}"
 			gcc -E $source | sed '/^#/ d' | sed '/^\s*$/d' | tr -d '\n' > $base.html
 		fi
@@ -29,7 +36,6 @@ if [ "$#" -eq 1 ]; then
 else
 	# Check for all the sources
 	for source in "$@"; do
-		echo "Found source $source"
 		base="${source/.html.h}"
 		gcc -E $source | sed '/^#/ d' | sed '/^\s*$/d' | tr -d '\n' > $base.html
 	done
